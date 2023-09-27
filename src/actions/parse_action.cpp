0bb0c5d54a2e62a28dee59a0001712999e758e79
ob-teau <alice.coinus@gmx.fr>
2023-09-27 15:13:07 +0200

    ctrl d toujours

--- src/actions/parse_action.cpp
+++ src/actions/parse_action.cpp
@@ -17,8 +17,32 @@ void Server::init_map_action()
 	_map_cmd["topic"] = &Server::do_action_topic;
 	_map_cmd["mode"] = &Server::do_action_mode;
 }
+
+/*void	Server::handle_client(int client_fd)
+{
+	char tmp[100] = {0};
+	int r = recv(client_fd, tmp, 100, 0); //recevoir jusque 100 octet de données de Client_fd
+	std::cout << "recv = '" << tmp << "'" << std::endl;
+	if (r == -1) {
+		perror("Error while receiving data.")
+		return;}
+	else if (r == 0) { //si r = 0 alors il n'ya pas/plus de connexion
+		client_disconnect(client_fd, _epollfd); //CLIENT DISCONNEXION
+		return;}
+	_map_client[client_fd]->getMsg().append(tmp, r);//ajoute les données recue 'tmp' a r.
+	size_t newLine = _map_client[client_fd]->getMs().find("\r\n");
+	if (newLine == std::string::npos) { //npos = une constante speciale qui dit que find n'a pas trouvé ce qu'il cherchait
+		newline = _map_client[client_fd]->getMsg().find("\n");}
+	if (newLine !=std::string::npos){
+		std::string message = _map_client[client_fd]->getMsg().substr(0, newLine);
+		execMsg(_map_client[client_fd], message);}
+	
+	//supprimer la partie du message traitée
+}*/
+
 void Server::parse_action(std::string message, Client *client)
 {
+//	handle_client(client._fd);
 	std::string firstWord = message.substr(0, message.find(' '));
 	for (int i = 0; i < firstWord.length(); i++)
 		firstWord.at(i) = (char) std::tolower(firstWord.at(i));
